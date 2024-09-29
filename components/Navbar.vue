4187bf604bd6d86035db24e174b951efd6b4be21
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-09-29 17:33:23 +0700

    add navbar and some pages

---
+++ components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav>
+    <ul class="nav-menu">
+      <li><nuxt-link to="/">Home</nuxt-link></li>
+      <li><nuxt-link to="/about">About</nuxt-link></li>
+      <li><nuxt-link to="/test">Test</nuxt-link></li>
+    </ul>
+  </nav>
+</template>
