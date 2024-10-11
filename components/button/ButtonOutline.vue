a6370884de19cec07e1220ea51cb342f55241028
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-10-11 21:17:40 +0700

    buttons responsive

--- components/button/ButtonOutline.vue
+++ components/button/ButtonOutline.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     :class="[
-      'btn-outline w-96 border-2 border-white/60 bg-transparent text-[#F8EDE3] rounded-full text-center py-2 font-semibold transition-colors duration-300 ease-linear hover:bg-[#F8EDE3] hover:text-red-500',
+      'btn-outline lg:w-48 text-sm lg:text-base border-2 border-white/60 bg-transparent text-[#F8EDE3] rounded-full text-center py-2 font-semibold transition-colors duration-300 ease-linear hover:bg-[#F8EDE3] hover:text-red-500',
       widthClass,
     ]"
   >
