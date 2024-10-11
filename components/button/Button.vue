1716bcce187e13662230c08c854b4ab744cc7dd4
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-10-11 17:39:08 +0700

    buttons component

---
+++ components/button/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    :class="[
+      'btn bg-red-500 text-[#F8EDE3] flex justify-center rounded-full py-2  font-semibold shadow-[0_0_7px_8px_rgba(255,0,0,0.3)] transition-shadow duration-300 ease-linear hover:shadow-[0_0_7px_15px_rgba(255,0,0,0.3)]',
+      widthClass,
+    ]"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  width: {
+    type: String,
+  },
+});
+
+const widthClass = props.width;
+</script>
---
+++ components/button/ButtonOutline.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    :class="[
+      'btn-outline w-96 border-2 border-white/60 bg-transparent text-[#F8EDE3] rounded-full text-center py-2 font-semibold transition-colors duration-300 ease-linear hover:bg-[#F8EDE3] hover:text-red-500',
+      widthClass,
+    ]"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  width: {
+    type: String,
+  },
+});
+
+const widthClass = props.width;
+</script>
