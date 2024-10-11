f90ff6c30097322fa390a2092695e650c293a675
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-10-11 14:23:17 +0700

    install nuxt swiper

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "nuxt-swiper"],
   app: {
     head: {
       title: "Movies App",
@@ -12,5 +12,12 @@ export default defineNuxtConfig({
     public: {
       tmdbApiKey: process.env.TMDB_API_KEY,
     },
+  },
+  swiper: {
+    // Swiper options
+    //----------------------
+    // prefix: 'Swiper',
+    // styleLang: 'css',
+    // modules: ['navigation', 'pagination'], // all modules are imported by default
   },
 });
