58b6adc5ec7e4d1081cceac89358cdd1765a1607
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-10-15 11:21:26 +0700

    footer responsive

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,34 +1,37 @@
 <template>
   <div class="h-[16rem]">
     <div class="flex h-full">
-      <div class="w-1/2 bg-[#0f0f0f] py-8">
-        <div class="text-white container grid gap-y-4">
-          <div
-            class="text-md sm:text-xl lg:text-3xl flex font-bold gap-1 md:gap-2 mb-4"
-          >
-            <img src="/popcorn.svg" class="w-4 sm:w-6 lg:w-10 h-auto" />
-            awaMovies
-          </div>
-          <div class="flex gap-2 text-sm">
-            Made with <img src="/nuxt.svg" class="w-16 h-auto color-white" />
-          </div>
-          <div class="flex gap-2 text-sm">
-            Data provided by<img src="/tmdb.svg" class="w-20 h-auto" />
-          </div>
-          <div class="flex gap-2 text-sm">
-            Developer : Aufa Ikrimah
-            <Icon name="mdi:github" class="text-white text-xl" />
-            <Icon name="mdi:linkedin" class="text-white text-xl" />
-            <Icon name="mdi:semantic-web" class="text-white text-xl" />
-          </div>
-        </div>
-      </div>
+      <div class="w-1/2 bg-[#0f0f0f]"></div>
       <div
         class="w-1/2 bg-no-repeat bg-cover bg-center"
         :style="`background-image: linear-gradient(to right, #0f0f0f, rgba(0, 0, 0, 0)), url(${apiConfig.oriImg(
           movies?.backdrop_path || ''
         )})`"
       ></div>
+      <div class="absolute text-white container grid gap-y-4 py-8">
+        <div
+          class="text-md sm:text-xl lg:text-3xl flex font-bold gap-1 md:gap-2 mb-4"
+        >
+          <img src="/popcorn.svg" class="w-4 sm:w-6 lg:w-10 h-auto" />
+          awaMovies
+        </div>
+        <div class="flex gap-2 text-[0.6rem] lg:text-sm">
+          Made with
+          <img src="/nuxt.svg" class="w-12 lg:w-16 h-auto color-white" />
+        </div>
+        <div class="flex gap-2 text-[0.6rem] lg:text-sm">
+          Data provided by<img src="/tmdb.svg" class="w-16 lg:w-20 h-auto" />
+        </div>
+        <div class="flex gap-2 text-[0.6rem] lg:text-sm">
+          Developer : Aufa Ikrimah
+          <Icon name="mdi:github" class="text-white text-base lg:text-xl" />
+          <Icon name="mdi:linkedin" class="text-white text-base lg:text-xl" />
+          <Icon
+            name="mdi:semantic-web"
+            class="text-white text-base lg:text-xl"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
