5741ab8054068f46aba0cf837ac26ff9ef7eb98e
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-10-17 13:17:34 +0700

    fetch data for movie slides and update responsive slider

--- components/movies/MovieSlides.vue
+++ components/movies/MovieSlides.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="movie-list">
+  <div class="container">
+    <div class="flex justify-between">
+      <div>1</div>
+      <div>2</div>
+    </div>
     <client-only>
       <Swiper :space-between="10" :slides-per-view="'auto'">
-        <SwiperSlide v-for="(item, index) in items" :key="index">
+        <SwiperSlide v-for="(item, index) in movies" :key="index">
           <MovieCard :item="item" :category="category" />
         </SwiperSlide>
       </Swiper>
@@ -14,32 +18,51 @@
 import MovieCard from "./MovieCard.vue";
 
 const props = defineProps({
-  items: {
-    type: Array,
-    default: () => [],
-  },
   category: String,
 });
+
+import { apiConfig } from "@/services/tmdbApi";
+import tmdbApi from "@/services/tmdbApi";
+
+const movies = ref([]);
+const loading = ref(true);
+const error = ref(null);
+
+const fetchMovies = async () => {
+  try {
+    const response = await tmdbApi.getMoviesList("popular");
+    if (response && response.results) {
+      movies.value = response.results;
+    } else {
+      error.value = "No movies found.";
+    }
+  } catch (err) {
+    error.value = "Error fetching movies.";
+    console.error("Error fetching movies:", err);
+  } finally {
+    loading.value = false;
+  }
+  // const data = await tmdbApi.person(137905)
+  // movies.value = data;
+  // const data = await tmdbApi.person(137905, "movie_credits");
+  // console.log(data.cast);
+};
+
+fetchMovies();
 </script>
 
 <style scoped>
-.movie-list {
-  @apply flex gap-4;
-}
-
 .swiper-slide {
   width: 15%;
 }
-
 @media (max-width: 768px) {
   .swiper-slide {
-    width: 30%;
+    width: 20%;
   }
 }
-
 @media (max-width: 640px) {
   .swiper-slide {
-    width: 40%;
+    width: 30%;
   }
 }
 </style>
