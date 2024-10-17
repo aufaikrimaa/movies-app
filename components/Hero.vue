e802ada860bd1f0a678bdb71099d7b0377438e7d
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-10-17 16:47:14 +0700

    update hero

--- components/Hero.vue
+++ components/Hero.vue
@@ -43,18 +43,18 @@
       >
         <SwiperSlide v-for="movie in movies" :key="movie.id">
           <div
-            class="h-[20rem] sm:h-[24rem] md:h-[32rem] xl:h-[40rem] 2xl:h-[48rem] bg-no-repeat bg-cover bg-center grid content-end justify-items-end py-4 lg:py-20"
+            class="h-[20rem] sm:h-[24rem] md:h-[32rem] xl:h-[40rem] 2xl:h-[48rem] bg-no-repeat bg-cover bg-center grid content-end py-4 lg:py-20"
             :style="`background-image: linear-gradient(to top, #0f0f0f, rgba(0, 0, 0, 0)), url(${apiConfig.oriImg(
               movie.backdrop_path
             )})`"
           >
-            <div class="container text-white grid justify-items-end">
+            <div class="container text-white">
               <div
                 class="bg-gray-300/20 py-1 px-2 lg:py-2 lg:px-3 w-fit rounded-full text-[0.5rem] lg:text-xs mb-4 lg:mb-6"
               >
                 Trending Movie
               </div>
-              <div class="w-11/12 md:w-4/5 lg:w-2/3 mb-5 lg:mb-7 text-end">
+              <div class="w-11/12 md:w-4/5 lg:w-2/3 mb-5 lg:mb-7">
                 <h1 class="text-3xl lg:text-6xl font-bold mb-3 lg:mb-6">
                   {{ movie.title }}
                 </h1>
