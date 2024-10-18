c2147883bfd001256129890b0bcfdbad5ed14cf5
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-10-18 10:05:04 +0700

    update navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,46 +1,60 @@
 <template>
   <div>
     <nav class="hidden lg:block absolute container z-20 top-9">
-      <ul class="text-white text-2xl flex gap-4 font-bold">
-        <li>
+      <div class="text-white text-2xl flex gap-6 font-bold">
+        <div>
           <nuxt-link to="/">Home</nuxt-link>
-        </li>
-        <li>
+        </div>
+        <div>
           <nuxt-link to="/">Movies</nuxt-link>
-        </li>
-        <li>
+        </div>
+        <div>
           <nuxt-link to="/">TV Shows</nuxt-link>
-        </li>
-        <li>
+        </div>
+        <div>
           <nuxt-link to="/">Favorites</nuxt-link>
-        </li>
-      </ul>
+        </div>
+      </div>
     </nav>
     <nav
       class="block lg:hidden w-4/5 fixed bottom-0 left-1/2 transform -translate-x-1/2 z-20"
     >
-      <ul class="nav-menu bg-gray-300/50 rounded-2xl flex justify-around mb-4">
-        <li>
-          <nuxt-link to="/">
-            <Icon name="mdi:home" class="text-white text-4xl" />
+      <div
+        class="nav-menu bg-gray-300/50 rounded-2xl flex justify-around mb-4 h-8 sm:h-10 md:h-12"
+      >
+        <div class="flex items-center">
+          <nuxt-link to="/" class="flex items-center justify-center">
+            <Icon
+              name="mdi:home"
+              class="text-white text-2xl sm:text-3xl md:text-4xl"
+            />
           </nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/">
-            <Icon name="mdi:movie-play" class="text-white text-4xl"
-          /></nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/">
-            <Icon name="mdi:tv-classic" class="text-white text-4xl"
-          /></nuxt-link>
-        </li>
-        <li>
-          <nuxt-link to="/">
-            <Icon name="mdi:favorite" class="text-white text-4xl"
-          /></nuxt-link>
-        </li>
-      </ul>
+        </div>
+        <div class="flex items-center">
+          <nuxt-link to="/" class="flex items-center justify-center">
+            <Icon
+              name="mdi:movie-play"
+              class="text-white text-2xl sm:text-3xl md:text-4xl"
+            />
+          </nuxt-link>
+        </div>
+        <div class="flex items-center">
+          <nuxt-link to="/" class="flex items-center justify-center">
+            <Icon
+              name="mdi:tv-classic"
+              class="text-white text-2xl sm:text-3xl md:text-4xl"
+            />
+          </nuxt-link>
+        </div>
+        <div class="flex items-center">
+          <nuxt-link to="/" class="flex items-center justify-center">
+            <Icon
+              name="mdi:favorite"
+              class="text-white text-2xl sm:text-3xl md:text-4xl"
+            />
+          </nuxt-link>
+        </div>
+      </div>
     </nav>
   </div>
 </template>
