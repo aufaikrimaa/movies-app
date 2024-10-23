a2843b6eca60751fd2234f1d892e162852049332
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-10-23 16:00:32 +0700

    responsive button menu on tv page

--- pages/tv/index.vue
+++ pages/tv/index.vue
@@ -1,31 +1,35 @@
 <template>
   <div class="bg-[#0f0f0f]">
     <Navbar />
-    <div class="flex justify-end pt-28 pb-8">
-      <div class="container flex gap-6">
+    <div class="flex lg:justify-end pt-28 pb-8">
+      <div class="container flex space-x-1 sm:space-x-4">
         <ButtonOutline
-          width="w-40"
+          width="w-24 sm:w-32 md:w-40"
           @click="changeType('popular')"
+          class="text-[0.6rem] sm:text-xs md:text-md lg:text-base"
           :class="{
             'bg-[#F8EDE3] text-red-500': cate === 'popular',
           }"
           >Popular</ButtonOutline
         >
         <ButtonOutline
-          width="w-40"
+          width="w-24 sm:w-32 md:w-40"
           @click="changeType('top_rated')"
+          class="text-[0.6rem] sm:text-xs md:text-md lg:text-base"
           :class="{ 'bg-[#F8EDE3] text-red-500': cate === 'top_rated' }"
           >Top Rated</ButtonOutline
         >
         <ButtonOutline
-          width="w-40"
+          width="w-24 sm:w-32 md:w-40"
           @click="changeType('airing_today')"
+          class="text-[0.6rem] sm:text-xs md:text-md lg:text-base"
           :class="{ 'bg-[#F8EDE3] text-red-500': cate === 'airing_today' }"
           >Airing Today</ButtonOutline
         >
         <ButtonOutline
-          width="w-40"
+          width="w-24 sm:w-32 md:w-40"
           @click="changeType('on_the_air')"
+          class="text-[0.6rem] sm:text-xs md:text-md lg:text-base"
           :class="{ 'bg-[#F8EDE3] text-red-500': cate === 'on_the_air' }"
           >On The Air</ButtonOutline
         >
