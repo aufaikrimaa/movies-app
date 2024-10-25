1e3cc9e1d691c0ccc0559d12488162851a3b0bfa
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-10-25 18:29:46 +0700

    slicing detail page

--- pages/tv/[id].vue
+++ pages/tv/[id].vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    Detail tv
-    <div>ID: {{ tvId }}</div>
+  <div class="bg-[#0f0f0f]">
+    <div class="base pt-16 sm:pt-20 md:pt-24 lg:pt-28 pb-8 text-white">
+      Detail tv
+      <div>ID: {{ tvId }}</div>
+    </div>
   </div>
 </template>
 
