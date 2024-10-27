35716582ead342d3c8ad7af7381c6fffe48d6fe0
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-10-27 11:34:12 +0700

    update space

--- components/movies/MovieInfo.vue
+++ components/movies/MovieInfo.vue
@@ -5,42 +5,42 @@
     </div>
     <div class="flex gap-12 w-full">
       <div class="w-1/4">
-        <div class="flex space-x-4">
+        <div class="flex space-x-2">
           <span class="label">Director </span>: <span>{{}}</span>
         </div>
-        <div class="flex space-x-4">
+        <div class="flex space-x-2">
           <span class="label">Status</span>:
           <span>{{ props.item.status }}</span>
         </div>
-        <div class="flex space-x-4">
+        <div class="flex space-x-2">
           <span class="label">Released Date</span>:
           <span>{{ props.item.release_date }}</span>
         </div>
 
-        <div class="flex space-x-4">
+        <div class="flex space-x-2">
           <span class="label">Runtime</span>:
           <span>{{ props.runtime }}</span>
         </div>
-        <div class="flex space-x-4">
+        <div class="flex space-x-2">
           <span class="label">Language</span>:
           <span>{{ props.item.original_language }}</span>
         </div>
       </div>
       <div class="w-2/3">
-        <div class="flex space-x-4">
+        <div class="flex space-x-2">
           <span class="label">Production</span>:
           <span>{{ props.item.production_companies[0].name }}</span>
         </div>
 
-        <div class="flex space-x-4">
+        <div class="flex space-x-2">
           <span class="label">Budget</span>:
           <span>{{ props.item.budget }}</span>
         </div>
-        <div class="flex space-x-4">
+        <div class="flex space-x-2">
           <span class="label">Revenue</span>:
           <span>{{ props.item.revenue }}</span>
         </div>
-        <div class="flex space-x-4">
+        <div class="flex space-x-2">
           <span class="label">Genres</span>:
           <div class="" v-for="(item, index) in props.item.genres" :key="index">
             <div
@@ -50,7 +50,7 @@
             </div>
           </div>
         </div>
-        <div class="flex space-x-4">
+        <div class="flex space-x-2">
           <span class="label">Official site</span>:
           <span>{{ props.item.homepage }}</span>
         </div>
