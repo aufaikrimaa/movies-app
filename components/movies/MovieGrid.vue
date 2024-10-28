3d284f085cb4fd83a9db02e6450e81e8ac1ccbbc
aufaikrimaa <aufaikrimah1214@gmail.com>
2024-10-28 16:33:00 +0700

    update grid

--- components/movies/MovieGrid.vue
+++ components/movies/MovieGrid.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="base">
     <div
-      class="grid gap-4 grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 grid-rows-4"
+      class="grid gap-2 sm:gap-3 grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 gap-y-6 sm:gap-y-8"
     >
       <div v-for="(item, index) in movies" :key="index">
         <MovieCard :item="item" :category="category" />
       </div>
     </div>
+
+    <div class="flex justify-center mt-8">
+      <ButtonOutline
+        v-if="!loading && !isLastPage"
+        @click="loadMoreMovies"
+        width="w-20 sm:w-28 md:w-36"
+      >
+        Load More
+      </ButtonOutline>
+    </div>
+
+    <!-- Loading Indicator -->
+    <div v-if="loading" class="text-center my-4">Loading...</div>
+
+    <!-- Error Message -->
+    <div v-if="error" class="text-center text-red-500">{{ error }}</div>
   </div>
 </template>
 
@@ -17,24 +33,32 @@ import tmdbApi from "@/services/tmdbApi";
 const props = defineProps({
   category: String,
   type: String,
-  title: String,
 });
 
 const movies = ref([]);
-const loading = ref(true);
+const loading = ref(false);
 const error = ref(null);
+const page = ref(1);
+const totalPages = ref(1);
+const isLastPage = computed(() => page.value >= totalPages.value);
 
-const fetchMovies = async () => {
+const fetchMovies = async (loadMore = false) => {
+  loading.value = true;
   try {
     let response;
     if (props.category === "movie") {
-      response = await tmdbApi.getMoviesList(props.type);
+      response = await tmdbApi.getMoviesList(props.type, { page: page.value });
     } else if (props.category === "tv") {
-      response = await tmdbApi.getTvList(props.type);
+      response = await tmdbApi.getTvList(props.type, { page: page.value });
     }
 
     if (response && response.results) {
-      movies.value = response.results;
+      if (loadMore) {
+        movies.value = [...movies.value, ...response.results];
+      } else {
+        movies.value = response.results;
+      }
+      totalPages.value = response.total_pages;
     } else {
       error.value = "No movies found.";
     }
@@ -46,5 +70,21 @@ const fetchMovies = async () => {
   }
 };
 
-watch(() => props.type, fetchMovies, { immediate: true });
+// Load more movies (increment page and fetch)
+const loadMoreMovies = () => {
+  if (page.value < totalPages.value) {
+    page.value += 1;
+    fetchMovies(true); // Load more, keep previous movies
+  }
+};
+
+// Fetch movies when type changes or on initial load
+watch(
+  () => props.type,
+  () => {
+    page.value = 1; // Reset to first page on type change
+    fetchMovies();
+  },
+  { immediate: true }
+);
 </script>
